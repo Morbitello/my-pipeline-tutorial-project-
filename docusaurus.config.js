91270445fc21920a98ade357a97b92f718c1cd4d
Morbitello <matteo.orbitello@gmail.com>
2024-11-14 09:56:39 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool from dev branch',
+  tagline: 'Dinosaurs are cool from dev branch test mergereq',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
