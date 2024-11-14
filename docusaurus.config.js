c86e20a98279ac1aa4d02050bdffc94c78e4799e
matteoorbitello <matteoorbitello@localhost.localdomain>
2024-11-14 10:44:30 +0100

    testo modified from main part 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool froma main',
+  tagline: 'Dinosaurs are cool froma main seconda volta',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
